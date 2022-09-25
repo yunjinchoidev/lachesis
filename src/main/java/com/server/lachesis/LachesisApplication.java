6cf5e290912c6c24928a72f70da0d8b580aef690
yunjin Choi <89494907+yunjinchoidev@users.noreply.github.com>
2022-09-25 13:31:43 +0900

    Initial commit

---
+++ src/main/java/com/server/lachesis/LachesisApplication.java
@@ -0,0 +1,13 @@
+package com.server.lachesis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LachesisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LachesisApplication.class, args);
+    }
+
+}
